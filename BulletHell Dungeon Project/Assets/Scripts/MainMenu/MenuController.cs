2b27fe62094a970e16056d246da6844dde8df720
derekchoi2 <derek@derekchoi.me>
2017-10-05 18:29:45 +0100

    Single scene menu system Following the ideas presented in the unity main menu tutorial

--- BulletHell Dungeon Project/Assets/Scripts/MainMenu/MenuController.cs
+++ BulletHell Dungeon Project/Assets/Scripts/MainMenu/MenuController.cs
@@ -5,50 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
-	enum Scenes{
-		MainMenu, Play, Help
-	}
-
-	public void ButtonClick(GameObject gameObject){
-		switch (gameObject.tag) {
-		case "MainMenuButton":
-			MainMenuButton ();
-			break;
-		case "PlayButton":
-			PlayButton ();
-			break;
-		case "HelpButton":
-			HelpButton ();
-			break;
-		case "QuitButton":
-			QuitButton ();
-			break;
-		}
-
-	}
-
-	void MainMenuButton(){
-		Debug.Log ("MainMenuButton clicked");
-		SceneManager.LoadScene ((int)Scenes.MainMenu);
-	}
-
-	void PlayButton(){
+	public void PlayButton(){
 		Debug.Log ("PlayButton clicked");
-		SceneManager.LoadScene ((int)Scenes.Play);
+		SceneManager.LoadScene (1);
 	}
 
-	void QuitButton(){
+	public void QuitButton(){
 		Debug.Log ("QuitButton clicked");
 		#if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
 		#else
 		Application.Quit ();
 		#endif
-	}
-
-	void HelpButton(){
-		Debug.Log ("HelpButton clicked");
-		SceneManager.LoadScene ((int)Scenes.Help);
 	}
 
 }
